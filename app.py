a31ef17194b62f9dfb89af77eb5124d324f27c0e
NESRINE CHAOUANI <nesrine.chaouani@insat.ucar.tn>
2024-12-03 16:33:20 +0100

    new file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -7,4 +8,4 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    serve(app, host='0.0.0.0', port=5000)

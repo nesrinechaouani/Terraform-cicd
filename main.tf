3b1293e368adc39d89bb6d965601cdf47ec2290c
nesrinechaouani <141138693+nesrinechaouani@users.noreply.github.com>
2024-12-07 22:31:58 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,17 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.67.0"
+      version = "~> 4.12.0"
     }
   }
 }
+
 provider "azurerm" {
   features {}
   subscription_id = "304799ce-2258-416d-85f2-8c42149f7550"
   tenant_id       = "dbd6664d-4eb9-46eb-99d8-5c43ba153c61"
+  client_id       =7ed1097c-36a3-4b50-9bc6-c0b2f91968fc
+  client_secret   =S-88Q~ppK10dMxkusnTJS4L8wlvDff3CagXRTazg
 }
 
 resource "azurerm_resource_group" "example" {

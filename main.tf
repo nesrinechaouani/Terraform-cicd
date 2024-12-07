af62d626085ac4c89a12405ad03dccf5195b20f7
nesrinechaouani <141138693+nesrinechaouani@users.noreply.github.com>
2024-12-07 23:23:03 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "azurerm" {
   features {}
   subscription_id = "304799ce-2258-416d-85f2-8c42149f7550"
   client_id       = "7ed1097c-36a3-4b50-9bc6-c0b2f91968fc"
-  client_secret   = "S-88Q~ppK10dMxkusnTJS4L8wlvDff3CagXRTazg"
+  client_secret   = "T7O8Q~XyRzigy6S5PkFnBEdYFUxu5wo4CMCHiagF"
   tenant_id       = "dbd6664d-4eb9-46eb-99d8-5c43ba153c61"
 
 }

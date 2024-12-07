fe608793f19c1821685cf581ab5861b8124c6f01
nesrinechaouani <141138693+nesrinechaouani@users.noreply.github.com>
2024-12-07 22:34:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,9 +10,10 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "304799ce-2258-416d-85f2-8c42149f7550"
+  client_id       = "7ed1097c-36a3-4b50-9bc6-c0b2f91968fc"
+  client_secret   = "S-88Q~ppK10dMxkusnTJS4L8wlvDff3CagXRTazg"
   tenant_id       = "dbd6664d-4eb9-46eb-99d8-5c43ba153c61"
-  client_id       =7ed1097c-36a3-4b50-9bc6-c0b2f91968fc
-  client_secret   =S-88Q~ppK10dMxkusnTJS4L8wlvDff3CagXRTazg
+
 }
 
 resource "azurerm_resource_group" "example" {

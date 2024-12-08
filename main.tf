f246b19e8c69735d181f634635a151015ce8e2d5
nesrinechaouani <141138693+nesrinechaouani@users.noreply.github.com>
2024-12-08 20:21:30 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "azurerm_app_service_plan" "example" {
 }
 
 #resource "azurerm_app_service" "example" {
-#  name                = "myAppService1"
+#  name                = "myAppService4"
 #  location            = azurerm_resource_group.example.location
 #  resource_group_name = azurerm_resource_group.example.name
 #  app_service_plan_id = azurerm_app_service_plan.example.id
@@ -43,7 +43,7 @@ resource "azurerm_app_service_plan" "example" {
 
 
 resource "azurerm_app_service" "example" {
-  name                = "myAppService1"
+  name                = "myAppService4"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id

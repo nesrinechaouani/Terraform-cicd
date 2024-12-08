80e4b48b9c440149ece8a5be2ddc637b706a40a4
nesrinechaouani <141138693+nesrinechaouani@users.noreply.github.com>
2024-12-08 22:05:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World!   Welcome to INSAT Promotion 2025'
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=5000)
